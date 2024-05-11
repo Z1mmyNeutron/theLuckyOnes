2f14ce029ea203a6bb7c5a4cf0bfee039fcdb592
Chrissie Zimmer <czimmer12@aol.com>
2024-05-10 20:00:39 -0400

    pages

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,15 @@ function App() {
 
   return (
     <div className="App">
+      <h1>
+        <div>
+          <ul>
+            <li><a href="/pages/home.js">Home</a></li>
+            <li><a href="/pages/about.js">About</a></li>
+            <li><a href="/pages/poetry.js">Poems</a></li>
+          </ul>
+        </div>
+      </h1>
       <h1>{message}</h1>
     </div>
   );
